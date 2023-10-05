584bf0bfc846b3b6e4d651bb4a3ee1f15d2f12ad
justin <justincoppola6@gmail.com>
2023-10-05 16:34:54 -0400

    finally done

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+// index.js
 
-// Write your code here
+function multiply(num1, num2) {
+    return num1 * num2;
+  }
+  
+  function random() {
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+  }
+  
+  function mod(num1, num2) {
+    return num1 % num2;
+  }
+  
+  function max(numbers) {
+    return Math.max(...numbers);
+  }
+  
+  module.exports = {
+    multiply,
+    random,
+    mod,
+    max,
+  };
+   
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,32 +1,26 @@
-require ( './helpers.js' );
+// indexTest.js
 
-const chai = require('chai')
-const expect = chai.expect;
+const assert = require('assert');
+const indexFunctions = require('../index');
 
-describe('index.js', function() {
-  describe('multiply', function() {
-    it("is an equation whose multiplied results will equal 62", function() {
-      expect(num1).to.be.a('number')
-      expect(num2).to.be.a('number')
-      expect(multiply).to.eq(62)
-    });
+describe('index.js', () => {
+  it('multiply is an equation whose multiplied results will equal 62', () => {
+    const result = indexFunctions.multiply(31, 2);
+    assert.strictEqual(result, 62);
   });
-  describe('random', function() {
-    it("generates a random integer greater than 0", function() {
-      expect(Number.isInteger(random)).to.be.true
-      expect(random).to.not.eq(0)
-    });
+
+  it('random generates a random integer greater than 0', () => {
+    const result = indexFunctions.random();
+    assert.ok(result > 0, `Expected ${result} to be greater than 0`);
   });
-  describe('mod', function() {
-    it("is an equation that calculates a remainder that is equal to 4", function() {
-      expect(num3).to.be.a('number')
-      expect(num4).to.be.a('number')
-      expect(mod).to.eq(4)
-    });
+
+  it('mod is an equation that calculates a remainder that is equal to 4', () => {
+    const result = indexFunctions.mod(14, 5);
+    assert.strictEqual(result, 4);
   });
-  describe('max', function() {
-    it("will return 20 as the highest number in the set", function() {
-      expect(max).to.eq(20)
-    });
+
+  it('max will return 20 as the highest number in the set', () => {
+    const result = indexFunctions.max([10, 5, 20, 15]);
+    assert.strictEqual(result, 20);
   });
 });
